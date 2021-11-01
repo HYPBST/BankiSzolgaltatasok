fd7f02fa28c7d0bddd1fc0ba2f7d74728c04012b
Gergő <s.gergo.az@gmail.com>
2021-11-01 17:38:07 +0100

    Szamla osztaly elkészítése

--- src/com/company/Szamla.java
+++ src/com/company/Szamla.java
@@ -1,11 +1,21 @@
 package com.company;
 
-public class Szamla extends BankiSzolgaltatas{
+    public abstract class Szamla extends BankiSzolgaltatas{
+
+
     private int aktualisEgyenleg;
 
 
-    public Szamla(Tulajdonos tulajdonos,int aktualisEgyenleg) {
+    public Szamla(Tulajdonos tulajdonos, int aktualisEgyenleg) {
         super(tulajdonos);
         this.aktualisEgyenleg = aktualisEgyenleg;
     }
+    public int getAktualisEgyenleg() {
+        return aktualisEgyenleg;
+    }
+    public void befizet(int osszeg){
+        this.aktualisEgyenleg+=osszeg;
+    }
+
+    abstract boolean kivesz(int osszeg);
 }
